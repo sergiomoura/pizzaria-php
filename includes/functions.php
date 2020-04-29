e29c18f7c04f9a69f1839b53d28f47533088d9f8
Sérgio Moura <smouracalmon@gmail.com>
2020-04-29 09:08:39 -0300

    Assegurando que string vai ter conteúdo antes de salva-la no usuarios.json

--- includes/functions.php
+++ includes/functions.php
@@ -155,13 +155,13 @@
      * Defina uma função que impima as informações de uma pizza
      */
     function pizzaPrint($pizza){}
-
+    echo('ok');
     /**
      * Carrega os usuários do arquivo usuarios.json
      * e retorna um array associativo contendo os usuários
      */ 
     function carregaUsuarios(){
-
+        echo('add usuario executada');
         // Ler o arquivo para uma variável string
         $strJson = file_get_contents("includes/usuarios.json");
 
@@ -179,7 +179,7 @@
     function addUsuario($nome, $telefone, $email, $endereço, $senha, $imagem){
         //carrega usuarios usando a função anterior
         $usuarios = carregaUsuarios();
-
+        
         //cria um array associativo $u com os dados passados por parâmetro
         $u = ['nome'=>$nome, 'telefone'=>$telefone, 'email'=>$email, 'endereço'=>$endereço, 'senha'=>$senha, 'imagem'=>$imagem];
 
@@ -189,10 +189,11 @@
         //transforma o array de usuários de volta em string json
         $stringjson = json_encode($usuarios);
 
-        //salva a string json no arquivo usuarios.json
-        file_put_contents('includes/usuarios.json', $stringjson);
+        // Verificando se existe algum caractere na stringjson.
+        // se tiver, salva no arquivo usuarios.json
+        if($stringjson){
+            //salva a string json no arquivo usuarios.json
+            file_put_contents('includes/usuarios.json', $stringjson);
+        }
         
     }
-?>
-
-?>
--- teste.php
+++ teste.php
@@ -1,23 +1,23 @@
 <?php 
     // Ler conteúdo de um arquivo e retornar a string que
     // tem dentro desse arquivo
-    $string_json = file_get_contents('includes/usuarios.json');
+    // $string_json = file_get_contents('includes/usuarios.json');
 
-    $usuarios = json_decode($string_json,true);
-
-    var_dump($string_json);
-    var_dump($usuarios);
-    echo "<pre>";
-    print_r($usuarios);
-    echo "</pre>";
-
+    // $usuarios = json_decode($string_json,true);
+    // var_dump($string_json);
+    // var_dump($usuarios);
+    // echo "<pre>";
+    // print_r($usuarios);
+    // echo "</pre>";
     // Transformando $usuarios de volta para uma string json
-    $string_json = json_encode($usuarios);
+    // $string_json = json_encode($usuarios);
 
-    file_put_contents('includes/teste.txt',$string_json);
-
+    // file_put_contents('includes/teste.txt',$string_json);
+    
     include('includes/functions.php');
     echo "<pre>";
+    // addUsuario($nome,    $telefone,    $email,                     $endereço,             $senha, $imagem)
+    addUsuario('Natália','11-99999999','natalia@digitalhouse.com','Rua das Profissionais','123',null);
     print_r(carregaUsuarios());
     echo "</pre>";
 ?>
