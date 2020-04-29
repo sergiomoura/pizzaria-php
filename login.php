ae0c88f4686882c0ee790f99ff5ea1dcae394c9d
Sérgio Moura <smouracalmon@gmail.com>
2020-04-29 11:19:51 -0300

    Adicionado form de login

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <label for="email"><input id="email" name="email" type="text" placeholder="E-mail"></label>
+        <label for="senha"><input id="senha" name="senha" type="password" placeholder="Senha"></label>
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
